01cfd771d3144fbecd0236552d7bce97c3924405
hujunhao <hongpao@2dfire.com>
2019-02-11 16:35:21 +0800

    w

--- chuncangzhihe/app.js
+++ chuncangzhihe/app.js
@@ -5,34 +5,34 @@
  * await: 等待执行,只能在异步函数内执行
  */
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
 /*
  * 接口集合
  * */
-const getHomeInfo = require("./requester/getHomeInfo");
-const getMain = require("./requester/getMain");
+const getHomeInfo = require("./requester/getHomeInfo")
+const getMain = require("./requester/getMain")
 
 /*
  * 初始化
  * */
-const app = express();
+const app = express()
 
 /*
  * 配置静态资源访问
  * */
-app.use('/public', express.static('./public'));
-app.use('/css', express.static('./css'));
-app.use('/images', express.static('./images'));
+app.use('/public', express.static('./public'))
+app.use('/css', express.static('./css'))
+app.use('/images', express.static('./images'))
 
 /*
  * 异步请求获取数据，特殊处理
  * */
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
-}));
+}))
 
 /**
  * 接口处理集合
@@ -42,11 +42,11 @@ getMain(app)
 
 //启动服务
 app.listen(3001, () => {
-    console.log('**************************************************************');
-    console.log('**                                                          **');
-    console.log('**                                                          **');
-    console.log('** Ladies and Gentlemen Node服务已启动完毕，可以正常使用... **');
-    console.log('**                                                          **');
-    console.log('**                                                          **');
-    console.log('**************************************************************');
-});
+    console.log('**************************************************************')
+    console.log('**                                                          **')
+    console.log('**                                                          **')
+    console.log('** Ladies and Gentlemen Node服务已启动完毕，可以正常使用... **')
+    console.log('**                                                          **')
+    console.log('**                                                          **')
+    console.log('**************************************************************')
+})
--- chuncangzhihe/requester/getMain.js
+++ chuncangzhihe/requester/getMain.js
@@ -6,34 +6,49 @@ const path = require('path')
 const setCrossDomain = require('../setCrossDomain')
 
 const getMain = (app) => {
+
+    //主页接口处理
     app.get('/start_main', (req, res) => {
+        /**
+         * 异步处理完成与否的标志位
+         * [nba图片文件名]
+         */
+        let isFinish = [0]
+
+        //所有图片名称
+        let imagesAry = []
 
         //允许跨域设置
         setCrossDomain(res)
 
+        //获取图片路径
         let root = path.resolve(__dirname, '..')
 
-        let imagesAry = []
+        //异步获取所有图片名称
         fs.readdir(`${root}/images/nba/`, (err, files) => {
             if (!err) {
                 for (let v of files) {
                     imagesAry.push(v)
                 }
-                finish(res, {
-                    imagesAry
-                })
+
+                isFinish[0] = 1
+
+                if (!isFinish.includes(0))
+                    finish(res)
             } else {
 
             }
         })
-    })
-}
 
-const finish = (res, options) => {
-    //接口返回输出
-    res.send({
-        code: 1,
-        data: options.imagesAry || []
+        const finish = (res) => {
+            //接口返回输出
+            res.send({
+                code: 1,
+                data: {
+                    imagesAry
+                }
+            })
+        }
     })
 }
 
